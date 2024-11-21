bd32a251c7ffc89a0e40d882d5c1ecaea263af4c
amartt <98788616+amartt@users.noreply.github.com>
2024-11-21 16:55:36 -0500

    Updated identification for independent packages and code cleanup to reduce redundancy

--- cleanup_env.py
+++ cleanup_env.py
@@ -11,7 +11,6 @@ Requirements:
 """
 
 import subprocess
-import re
 from typing import Optional, Set
 
 def style_text(
@@ -59,17 +58,6 @@ def style_text(
 
     print(f"{ansi_start}{text}{ansi_end}")
 
-def is_valid_package_name(
-        line: str
-    ) -> bool:
-    """
-    Checks if a given line of text begins with a valid package name.
-
-    Begins with a letter, digit, underscore, or hyphen.
-    """
-    pattern = r'^[a-zA-Z0-9_-]'
-    return re.match(pattern, line) is not None
-
 def parse_requirements_file(
         filepath: str
     ) -> Set[str]:
@@ -83,46 +71,44 @@ def parse_requirements_file(
             packages.add(package_name)
     return packages
 
-def get_installed_packages() -> Set[str]:
-    """
-    Get all installed packages in the environment.
-    """
-    packages = set()
-    package_output = subprocess.run(
-        ["pip", "freeze"],
-        capture_output=True,
-        text=True,
-        check=True
-    )
+def get_packages(
+        case: str
+    ) -> Set[str]:
 
-    package_output_lines = package_output.stdout.splitlines()
-    for line in package_output_lines:
-        package_name = line.split("==")[0]
-        packages.add(package_name)
+    """
+    Get set of package names from the output of a subprocess.
+    """
+    # Excluded packages to not be returned in the results
+    # pipdeptree is a required package so it cannot be removed
+    excluded_packages = {
+        "pipdeptree"
+    }
+
+    # Define commands
+    commands = {
+        "requirements": ["pip", "freeze"],
+        "dependencies": ["pipdeptree", "-d", "0"]
+    }
+    command = commands.get(case)
+    if command is None:
+        raise ValueError(f"Invalid case: {case}")
     
-    return packages
-
-def get_independent_packages() -> Set[str]:
-    """
-    Get independent packages from the dependency tree of installed packages.
-    """
-    # Run subprocess to get dependency tree of installed packages
-    dep_tree_output = subprocess.run(
-        ["pipdeptree"],
+    # Run subprocess to get packages
+    subprocess_output = subprocess.run(
+        command,
         capture_output=True,
         text=True,
         check=True
     )
-    dep_tree_output_lines = dep_tree_output.stdout.splitlines()
 
-    # Identify all installed packages without any dependencies in the tree
-    independent_packages = set()
-    for line in dep_tree_output_lines:
-        if is_valid_package_name(line):
-            package_name = line.split("==")[0]
-            independent_packages.add(package_name)
+    # Get all packages without version info
+    packages = {
+        line.split("==")[0] 
+        for line in subprocess_output.stdout.splitlines() 
+        if line.split("==")[0] not in excluded_packages
+    }
 
-    return independent_packages
+    return packages
 
 def get_new_independent_packages(
         requirements_filepath: str
@@ -134,13 +120,13 @@ def get_new_independent_packages(
     current_packages = parse_requirements_file(requirements_filepath)
 
     # Get all installed packages in the environment
-    installed_packages = get_installed_packages()
+    installed_packages = get_packages(case="requirements")
 
     # Find all installed packages which are not included in the requirements file
     new_packages = installed_packages - current_packages
 
     # Identify all installed packages without any dependencies
-    dependency_free_packages = get_independent_packages()
+    dependency_free_packages = get_packages(case="dependencies")
 
     # Return all newly installed packages which have no dependencies
     return new_packages & dependency_free_packages
@@ -157,47 +143,54 @@ def main():
     packages_of_interest = get_new_independent_packages(
         requirements_filepath=req_filepath
     )
-    print(f"Independent packages: {packages_of_interest}")
+    if packages_of_interest:
+        print(f"Independent packages: {packages_of_interest}")
 
-    # Get user input to keep any amount of the new packages
-    packages_to_keep = set()
-    while True:
-        user_input = input("Enter a package to keep (type 'n' when finished): ").strip()
-        if user_input == 'n':
-            break
-        if user_input:
-            packages_to_keep.add(user_input)
+        # Get user input to keep any amount of the new packages
+        packages_to_keep = set()
+        while True:
+            user_input = input("Enter a package to keep (type 'n' when finished): ").strip()
+            if user_input == 'n':
+                break
+            if user_input:
+                packages_to_keep.add(user_input)
 
-    # Iteratively remove new packages that do not want to be kept and all unneeded dependencies
-    while True:
-        # Re-identify all new packages which do not have any dependencies
-        style_text(
-            text="\nRe-identifying independent packages...",
-            color="yellow",
-            bold=True
-        )
-        packages_of_interest = get_new_independent_packages(
-            requirements_filepath=req_filepath
-        )
-
-        # Uninstall each package without a confirmation
-        packages_to_uninstall = packages_of_interest - packages_to_keep
-        if packages_to_uninstall:
-            print(f"Packages to uninstall: {packages_to_uninstall}")
-            for package in packages_to_uninstall:
-                style_text(
-                    text=f"\nUninstalling {package}...",
-                    color="red",
-                    bold=True
-                )
-                subprocess.run(["pip", "uninstall", "-y", package])
-        else:
+        # Iteratively remove new packages that do not want to be kept and all unneeded dependencies
+        while True:
+            # Re-identify all new packages which do not have any dependencies
             style_text(
-                text="\nNo more packages to uninstall.",
-                color="green",
+                text="\nRe-identifying independent packages...",
+                color="yellow",
                 bold=True
             )
-            break
+            packages_of_interest = get_new_independent_packages(
+                requirements_filepath=req_filepath
+            )
+
+            # Uninstall each package without a confirmation
+            packages_to_uninstall = packages_of_interest - packages_to_keep
+            if packages_to_uninstall:
+                print(f"Packages to uninstall: {packages_to_uninstall}")
+                for package in packages_to_uninstall:
+                    style_text(
+                        text=f"\nUninstalling {package}...",
+                        color="red",
+                        bold=True
+                    )
+                    subprocess.run(["pip", "uninstall", "-y", package])
+            else:
+                style_text(
+                    text="\nNo more packages to uninstall.",
+                    color="green",
+                    bold=True
+                )
+                break
+    else:
+        style_text(
+            text="\nNo independent packages to keep or uninstall.",
+            color="green",
+            bold=True
+        )
 
 if __name__ == "__main__":
     main()
